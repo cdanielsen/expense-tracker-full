23469775470bf5ba856147bb4486b03f00b7a9ed
Diane Douglas & Christian Danielsen <student@epicodus.com>
2014-07-28 15:21:30 -0700

    Debugged the crap out of the js file. Program works robustly

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,67 @@
 var Category = {
-  items: [],
-}
+  initialize : function (name) {
+    this.items = [];
+    this.name = name;
+  }
+};
 
 var Purchase = {
+  initialize : function (description, amount, quantity) {
+    this.description = description;
+    this.dollarAmount = amount;
+    this.quantity = quantity;
+    this.purchaseTotalCost();
+  },
   purchaseTotalCost : function () {
     this.totalCost = this.dollarAmount * this.quantity;
   }
 };
 
-var currentCategory;
-var categoriesArr = [];
+
 
 $(document).ready(function(){
-  
+var currentCategory;
+
+  //CREATE A NEW CATEGORY
   $("#category-input").submit(function(event){
     event.preventDefault();
-    newCategory = Object.create(Category);
-    newCategory.name = $("#category").val();
-    console.log(newCategory);
-    currentCategory = newCategory.name;
-    categoriesArr.push(newCategory);
-    $("#category-list ul").append("<li class='clickable'>" + newCategory.name + "</li>");
-    $("span#name-category").text(currentCategory);
-    $("div#purchase-list td").empty();
+    var newCategory = Object.create(Category);
+    newCategory.initialize($("#category").val());
+    currentCategory = newCategory;
+
+    $("#category-list ul").append("<li class='clickable'>" + currentCategory.name + "</li>");
+    $("span#name-category").text(newCategory.name);
+    $("div#purchase-list tbody").empty();
     $("#category").val("");
+
+    // RECALL AN OLD CATEGORY
+    $(".clickable").last().click(function(){
+      currentCategory = newCategory;
+      $("span#name-category").text(currentCategory.name);
+      $("div#purchase-list tbody").empty();
+
+      currentCategory.items.forEach(function(purchase) {
+        $(".table tbody").append("<tr><td>" + purchase.description
+          + "</td><td>$" + purchase.dollarAmount
+          + "</td><td>" + purchase.quantity
+          + "</td><td>$" + purchase.totalCost
+          + "</td></tr>");
+      });
+    });
   });
 
+  //ADD NEW PURCHASES
   $("#item-input").submit(function(event){
     event.preventDefault();
-    newPurchase = Object.create(Purchase);
-    newPurchase.description = $("#description").val();
-    newPurchase.dollarAmount = $("#amount").val();
-    newPurchase.quantity = $("#quantity").val();
-    newPurchase.purchaseTotalCost();
+    var newPurchase = Object.create(Purchase);
+    newPurchase.initialize($("#description").val(), $("#amount").val(), $("#quantity").val());
+    // newPurchase.purchaseTotalCost();
+    currentCategory.items.push(newPurchase);
 
-    //goal for the following block: search the categories array, find the correct category based on category.name,
-    //and push the purchase into the correct category's "items" array
-    categoriesArr.forEach(function(category) {
-      if (category.name === currentCategory){
-        category.items.push(newPurchase)
-      }
-    })
-    console.log(categoriesArr);
 
     $(".table tbody").append("<tr><td>" + newPurchase.description + "</td><td>$" + newPurchase.dollarAmount + "</td><td>" + newPurchase.quantity + "</td><td>$" + newPurchase.totalCost + "</td></tr>");
     $("input#description").val("");
     $("input#amount").val("");
     $("input#quantity").val("");
   });
-
-  //In progress: old categories click event behaviors:
-    // --set the current category
-    // --search categoriesarray and find the match
-    // --update purchase header
-    // --populate purchaselist field with stored data
-
-  $("div#category-list li.clickable").click(function() {
-    alert("Yo!"); //listener not working =/
-    currentCategory = $(); //need text from click event ("this" or this.val()?!)
-    $("span#name-category").text(currentCategory);
-    
-    categoriesArr.forEach(function(category) {
-      if (category.name === currentCategory) {
-        category.items.forEach(function(item) {
-          $(".table tbody").append("<tr><td>" + item.description + "</td><td>$" + item.dollarAmount + "</td><td>" + item.quantity + "</td><td>$" + item.totalCost + "</td></tr>");
-        })
-      }
-    })
-  });
 });
-
-
-//Two main bugs:
- // -- on new category creation, even though new category is pushed into new spot in categoryArr, it is inheriting purchase items! (conditional flaw in forEach loop??)
- // -- on new purchase creation, purchases are being pushed into ALL categories, not just current
